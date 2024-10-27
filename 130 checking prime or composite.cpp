b3ab97dc4c8c670dd3460c071e24d7e8d9339a62
Sandip-Maity-2023 <12sandip125@gmail.com>
2024-10-28 00:51:03 +0530

    update

--- 130 checking prime or composite.cpp
+++ 130 checking prime or composite.cpp
@@ -1,3 +1,27 @@
 //
 // Created by 12san on 27-10-2024.
 //
+#include<iostream>
+using namespace std;
+class prime{
+public:
+   friend int func();
+};
+int func(int num,int i){
+    if(i==1){
+        return 1;
+    }else{
+        if(num%i==0){
+            return 0;
+        }else{
+            return func(num,i-1);
+        }
+    }
+}
+int main(){
+    int a;
+    cout<<"Enter the number: "<<endl;
+    cin>>a;
+    prime p;
+    p.
+}
