4f1b8fdd712087ae03e00e68c97e64d6f279b30a
Sandip-Maity-2023 <12sandip125@gmail.com>
2025-02-05 18:28:10 +0530

    updATE

---
+++ Projects/Hospital Accomodation System.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<mysql.h>
+#include<string>
+#include<mysql_error.h>
+#include<windows.h>
+#include<sstream>
+using namespace std;
+int main(){
+printf("Hello World");
+}
