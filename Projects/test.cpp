1a047147fd92a2143a6af0bccde242a9015ab8e8
Sandip-Maity-2023 <12sandip125@gmail.com>
2025-02-05 19:01:29 +0530

    update

---
+++ Projects/test.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+//#include<mysql.h>
+#include<string>
+//#include<mysql_error.h>
+//#include<windows.h>
+//#include<sstream>
+using namespace std;
+int main(){
+cout<<"Hello World";
+cout<<"Hello World";
+return 0;
+}
